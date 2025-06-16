cad58730282b7aae52333acb2db34b14e577fcb5
myagkow <e.myagkow@yandex.ru>
2025-06-16 12:35:12 +0500

    test3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
+import {useEffect} from "react";
 
 function App() {
-
-
+    useEffect(() => {
+        Telegram.WebApp.ready();
+        Telegram.WebApp.expand();
+    }, []);
   return (
     <>
       <div>Hello World</div>
